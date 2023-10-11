6e976d5c26267acd13c46b4df3ceabfda941a7d8
Saime-shahzad <58229214+Saime-shahzad@users.noreply.github.com>
2023-10-11 16:59:05 +0500

    update work

---
+++ Apis/updateapi.js
@@ -0,0 +1,50 @@
+const pool = require("../Utils/db");
+
+const express = require("express");
+const tableName = require("../tableNames/tableNames");
+const router = express.Router();
+
+router.get("/update" , (req, res) =>{
+  
+
+    let payload = {
+        id:1,
+
+        tableName: tableName.testing,
+      databasefields:{
+        userName:"khansaad"
+      }
+      };
+      let keys = Object.keys(payload.databasefields);
+      let values = Object.values(payload.databasefields);
+      console.log("keys>>", keys);
+      console.log("values>>", values);
+      try {
+          pool.getConnection((err , connection) => {
+              if (err){
+                  console.log("error");
+                }
+                let query = `UPDATE ${payload.tableName} SET  ${keys.join(
+                    " = ? ,"
+                    )} = ? where id = ${payload.id}`;
+connection.query(
+    query, values,
+(err , result) => {
+    connection.release()
+    if(err){
+        res.send("sorry")
+    }
+    else{
+res.send("success")
+    }
+
+})
+
+        })
+        
+    } catch (error) {
+        console.log("error");
+        
+    }
+})
+module.exports=router
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const createTestApi = require("../Apis/testApi");
 // const insertApi=require("../Apis/createApi")
 const insertApi = require("../Apis/createApi");
+const updateApi = require("../Apis/updateapi");
 
 router.use("/api/createTestApi", createTestApi);
 router.use("/api/testing/", insertApi);
+router.use("/api/update", updateApi);
 
 
 module.exports = router;
